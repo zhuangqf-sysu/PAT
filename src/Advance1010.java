29d14757f2f7d668746d14286c5461952fadb6ec
zhuangqf <zhuangqinfa@outlook.com>
2016-09-05 13:48:50 +0800

    Add 1010

--- src/Advance1010.java
+++ src/Advance1010.java
@@ -1,5 +1,8 @@
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 /**
  * Created by zhuangqf on 8/25/16.
@@ -18,23 +21,64 @@ public class Advance1010 {
             n2 = n;
         }
 
-        Long a = allToTen(n1,radix);
-        if(a.equals(-1L)){
+        long a = allToTen(n1,radix);
+        if(a==-1L){
             System.out.println("Impossible");
             return;
         }
 
-        Long b = 0L;
+        long b = 0L;
 
         int i = 2;
         for(;i<=50;i++){
             b = allToTen(n2,i);
-            if(b.equals(-1L)) continue;
-            if(b.equals(a)) break;
+            if(b==-1L) continue;
+            if(b==a) break;
         }
 
-        if(i<=50) System.out.println(i);
-        else System.out.println("Impossible");
+        if(i<=50) {
+            System.out.println(i);
+            return;
+        }
+
+        if(a<=50){
+            System.out.println("Impossible");
+            return;
+        }
+
+        BigInteger minRadix = new BigInteger("2");
+        BigInteger maxRadix = new BigInteger("50");
+        BigInteger MaxRadix = new BigInteger("-1");
+        BigInteger maybeAns = new BigInteger("-1");
+
+        BigInteger stopValue = new BigInteger("20");
+        BigInteger multValue = new BigInteger("2");
+        BigInteger A = new BigInteger(String.valueOf(a));
+        BigInteger B;
+        while(maxRadix.compareTo(minRadix.add(stopValue))>0) {
+            B = allToTen(n2,maxRadix);
+           // System.out.println(A+" "+B+" "+minRadix+" "+maxRadix+" "+MaxRadix+" "+maybeAns);
+            if(A.compareTo(B)>0){
+                minRadix = maxRadix;
+                if(MaxRadix.compareTo(BigInteger.ZERO)>0) maxRadix = MaxRadix;
+                else maxRadix = maxRadix.multiply(multValue);
+            }else{
+                MaxRadix = maxRadix;
+                maxRadix = maxRadix.add(minRadix).divide(multValue).add(multValue);
+                if(A.compareTo(B)==0&&
+                        (maybeAns.compareTo(BigInteger.ZERO)<0
+                                ||maybeAns.compareTo(maxRadix)>0))
+                    maybeAns = maxRadix;
+            }
+        }
+        for(;MaxRadix.compareTo(minRadix)>=0;
+            MaxRadix = MaxRadix.subtract(BigInteger.ONE)){
+            B = allToTen(n2,MaxRadix);
+            if(A.equals(B)) maybeAns = MaxRadix;
+        }
+
+        if(maybeAns.compareTo(BigInteger.ZERO)<0) System.out.println("Impossible");
+        else System.out.println(maybeAns);
     }
 
     private static Long allToTen(String n1, int radix) {
@@ -47,6 +91,18 @@ public class Advance1010 {
             if(a>=radix) return -1L;
 
             num = num * radix + a;
+        }
+        return num;
+    }
+
+    private static BigInteger allToTen(String n1, BigInteger radix) {
+        BigInteger num = new BigInteger("0");
+        for(int i=0;i<n1.length();i++){
+            char c = n1.charAt(i);
+            int a;
+            if(c<='z'&&c>='a') a = c - 'a' + 10;
+            else a = c - '0';
+            num = num.multiply(radix).add(new BigInteger(String.valueOf(a)));
         }
         return num;
     }
